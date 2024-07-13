440910136b6215c7a7a82821a5fd651ff8d84fb4
arafat219 <2105118@ugrad.cse.buet.ac.bd>
2024-07-14 02:30:14 +0600

    find a way to join user and community
    using js

--- server/controllers/Home/HomeController.js
+++ server/controllers/Home/HomeController.js
@@ -3,9 +3,30 @@ const {
 } = require('../../db/connect');
 
 const getCollections = async (req, res) => {
-    const collection = await getCollection('User');
-    console.log(collection);
-    res.json({ message: 'hehe' , collection: collection});
+    try {
+        const users = await getCollection('User'); 
+        const comms = await getCollection('Community');
+        var usercom = [];
+        for (var i = 0; i < users.length; i++) {
+            var temp = {
+                username: users[i].Name,
+                email: users[i].Email,
+                Community: []
+            }
+            comms.forEach(comm => {
+                if (comm.User.includes(users[i].UserID)) {
+                    temp.Community.push(comm.Name);
+                }
+            });
+            usercom.push(temp);
+        }
+        console.log(usercom);
+        res.json({ message: 'hehe', user : usercom });
+    } catch (error) {
+        console.error("Error sending response:", error);
+        res.status(400).json({ message: 'Internal Server Error', error: error.message });
+    }
 }
+
 
 module.exports = { getCollections };
