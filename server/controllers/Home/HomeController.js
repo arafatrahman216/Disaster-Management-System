8ac6be4095f5cbef0569e1c836a56d6111dc68b5
arafat219 <2105118@ugrad.cse.buet.ac.bd>
2024-07-14 13:53:59 +0600

    user and community join success
    used service

--- server/controllers/Home/HomeController.js
+++ server/controllers/Home/HomeController.js
@@ -1,13 +1,18 @@
-const {
-    getCollection
-} = require('../../db/connect');
+// const {
+//     getCollection
+// } = require('../../db/connect');
+// const User = require('../../models/User');
 
-const User = require('../../models/User');
+const { GetUserCommunity } = require('../../services/User/UserService');
 
 const getCollections = async (req, res) => {
-    const collection = await User.find({})
-    console.log(collection);
-    res.json({ message: 'hehe' , collection: collection});
+    try {
+        const usercom = await GetUserCommunity();
+        res.json({ message: 'hehe', user : usercom });
+    } catch (error) {
+        console.error("Error sending response:", error);
+        res.status(400).json({ message: 'Internal Server Error', error: error.message });
+    }
 }
 
 module.exports = { getCollections };
