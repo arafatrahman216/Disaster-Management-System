207f730c8bfe1c2cba88780923eb43aaeb57bf62
Tahmidul Islam Omi <tahmidulislamomi09@gmail.com>
2024-08-30 20:44:33 +0600

    Donation task done

--- server/controllers/Home.js
+++ server/controllers/Home.js
@@ -4,6 +4,26 @@ const Donation = require('../models/Donation');
 const Location = require('../models/Location');
 const EmergencyContact = require('../models/EmergencyContact');
 
+async function getTotalDonationAmount() {
+    try {
+        // Fetch all donations and sum up the amounts
+        const totalAmount = await Donation.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    totalAmount: { $sum: "$Amount" }
+                }
+            }
+        ]);
+
+        // If no donations, totalAmount will be 0
+        return totalAmount.length > 0 ? totalAmount[0].totalAmount : 0;
+    } catch (err) {
+        console.error('Error calculating total donation amount:', err);
+        throw err;
+    }
+}
+
 const home = async(req, res) => {
     try {
         // Step 1: Find all running incidents
@@ -19,8 +39,10 @@ const home = async(req, res) => {
 
         const contacts = await EmergencyContact.find({ locationID: { $in: locationIDs } });
 
+        const totalAmount = await getTotalDonationAmount();
+
         // Step 4: Send the retrieved locations in the response
-        res.status(200).json({ runningIncidents, locations, contacts });
+        res.status(200).json({ runningIncidents, locations, contacts, totalAmount });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
