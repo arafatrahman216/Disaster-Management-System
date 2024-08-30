8bfffddc13968db03bbe1f4208ee12112c8bc9ee
Tahmidul Islam Omi <tahmidulislamomi09@gmail.com>
2024-08-30 16:38:52 +0600

    ..

--- server/controllers/Incident.js
+++ server/controllers/Incident.js
@@ -1,9 +1,21 @@
 const Incident = require('../models/Incident');
+const Location = require('../models/Location');
 
 const getAllIncidents = async(req, res) => {
     try {
         const incidents = await Incident.find({});
-        res.status(200).json({incidents});
+
+        const incidentsWithLocation = await Promise.all(
+            incidents.map(async (incident) => {
+            const location = await Location.findById(incident.locationID);
+            return {
+                ...incident._doc, // Spread the incident details
+                location, // Add the location details
+            };
+            })
+        );
+
+        res.status(200).json({incidents: incidentsWithLocation});
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
