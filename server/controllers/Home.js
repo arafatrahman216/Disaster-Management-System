7c203e9eb909fb12cb11ac94ff1d5e29d0e09fca
Tahmidul Islam Omi <tahmidulislamomi09@gmail.com>
2024-08-30 12:12:45 +0600

    home backend working

--- server/controllers/Home.js
+++ server/controllers/Home.js
@@ -1,16 +1,28 @@
 const Incident = require('../models/Incident');
 const Community = require('../models/Community');
 const Donation = require('../models/Donation');
+const Location = require('../models/Location');
 
 const home = async(req, res) => {
     try {
+        // Step 1: Find all running incidents
         const runningIncidents = await Incident.find({ Status: "Running" });
-        res.status(200).json({ runningIncidents });
-    } catch (error) {
-        res.status(500).json({ message: 'Error fetching running incidents', error: error.message });
-    }
-
     
+        // Step 2: Extract all IncidentIDs from runningIncidents
+        const incidentIDs = runningIncidents.map(incident => incident.IncidentID);
+    
+        // Step 3: Use IncidentIDs to retrieve information from the Location collection
+        const locations = await Location.find({ IncidentID: { $in: incidentIDs } });
+
+        const locationIDs = locations.map(location => location.LocationID);
+
+        const contacts = await EmergencyContact.find({ locationID: { $in: locationIDs } });
+
+        // Step 4: Send the retrieved locations in the response
+        res.status(200).json({ runningIncidents, locations, contacts });
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching running incidents or locations', error: error.message });
+    }
     
 }
 
